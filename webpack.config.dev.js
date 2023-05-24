d648732fc17a0489f844788fda88f81756b52051
Zach Schneider <me@zachschneider.com>
2023-05-24 13:11:39 -0500

    fixed broken tests

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -16,21 +16,13 @@ module.exports = {
     open: true, // Open the browser automatically
     port: 8080, // Port number to run the server on
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
+  devtool: 'eval-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
     }),
     new DashboardPlugin({
-      // Custom options here
       customOptions: {
         logLevel: "info",
         minWidth: 500,
@@ -39,8 +31,7 @@ module.exports = {
         theme: {
           scheme: "monokai",
           author: "wimer hazenberg (http://www.monokai.nl)",
-          base00: "#272822", // example custom color
-          // more custom colors...
+          base00: "#272822", 
         },
       },
     }),
