11438426880d90ab7a78c9be3a3197e760911da7
Zach Schneider <me@zachschneider.com>
2026-01-07 06:00:12 -0600

    updated packages and jest

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     '^.+\\.js$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(parse5|entities|parse5-htmlparser2-tree-adapter)/)',
+    'node_modules/(?!(parse5|entities|parse5-htmlparser2-tree-adapter|@exodus/bytes)/)',
   ],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
